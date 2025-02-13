2ea9cbab4280571b06d0f16245ae7c51eef90c38
Ryfo Myory <ryfomyory@gmail.com>
2025-02-13 21:32:02 +0800

    feat:add memo feature include delete

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,194 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from 'vue'
+
+const showForm = ref(false)
+const newMemo = ref('');
+const memos = ref<{ id: number, content: string, date: string }[]>([]);
+const errorMessage = ref('');
+
+
+  const saveMemo = () => {
+    if(!newMemo.value) {
+      errorMessage.value = 'Please enter a memo';
+      return
+    }
+    memos.value.push({
+      id: Date.now(),
+      content: newMemo.value,
+      date: new Date().toLocaleDateString("en-GB"),
+    })
+    newMemo.value   = '';
+    showForm.value  = false;
+  }
+
+  const handleKeydown = () => {
+        showForm.value = false
+  };
+
+  const deleteCard = (id: number) => {
+    memos.value = memos.value.filter((memo) => memo.id !== id);
+  };
+
 </script>
 
 <template>
-  <div>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+  <main>
+    <div class="container">
+      <header>
+        <h1 class="header-title">Memo</h1>
+        <button @keydown.escape="handleKeydown()" @click="showForm = true" class="header-button">+</button>
+      </header>
+      <div class="card-container">
+        <div 
+        v-for="(memo) in memos" 
+        class="card" 
+        :key="memo.id"
+        >
+          <button @click="deleteCard(memo.id)" class="card-close-button">X</button>
+          <p class="card-content">{{ memo.content }}</p>
+          <p class="card-date">{{ memo.date }}</p>
+          
+        </div>
+      </div>
     </div>
-    <HelloWorld msg="Hello Vue" />
-  </div>
-</template>
+    <div v-if="showForm" class="form-overlay">
+      <div class="form-model">
+        <button @click="showForm = false"  class="form-close-button">&times;</button>
+        <span v-if="errorMessage" class="form-error">{{ errorMessage }}</span>
+        <textarea v-model="newMemo" name="memo" id="memo" rows="10"></textarea>
+        <button @click="saveMemo()" class="form-save-button">Save</button>
+      </div>
+    </div>
+  </main>
+  </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+main {
+  height: 100vh;
+  width: 100vw;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.container {
+  max-width: 900px;
+  padding: 10px;
+  margin: 0 auto;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+.header-title {
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #495a7d;
+}
+
+.header-button {
+  padding: 10px;
+  border: none;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 30%;
+  background-color: #495a7d;
+  color: white;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  gap: 105px;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #495a7d;
+  border-radius: 5%;
+  padding: 10px;
+  height: 210px;
+  width: 210px;
+  margin-bottom: 10px;
+  color: white;
+}
+.card:nth-child(3n) {
+  background-color: antiquewhite;
+  color: black;
+}
+.card:nth-child(3n-2) {
+  background-color: antiquewhite;
+  color: black;
+}
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.765);
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-model {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 15px;
+  color: white;  
+}
+
+.form-close-button {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  font-size: 25px;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+}
+
+.form-error {
+  color: red;
+  margin-bottom: 10px;
+}
+
+.card-close-button {
+  position: absolute;
+  top: 10px;
+  z-index: 1;
+  top: 5px;
+  width: 30px;
+  height: 30px;
+  font-size: 25px;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  color: black;
+}
+
+
 </style>
--- src/components/HelloVue.vue
+++
@@ -1,9 +0,0 @@
-<template lang="">
-  <div>
-      <h2></h2>
-  </div>
-</template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
